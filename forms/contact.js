91f2f336740c5eb5e3a7f75c3b0e753013d39429
Irfnmdlptra <bangasrat@gmail.com>
2023-01-25 17:21:20 +0800

    Menyelesaikannya

--- forms/contact.js
+++ forms/contact.js
@@ -4,19 +4,26 @@
       const form = document.forms["ofty-form"];
       const btnKirim = document.querySelector('.btn-kirim')
       const btnLoading = document.querySelector('.btn-loading')
+      const btnSubrek = document.querySelector('.btn-subrek')
       const myAlert = document.querySelector('.my-alert')
+      
+      btnKirim.addEventListener('click', function(){
+        alert('cobabab')
+      })
 
       form.addEventListener("submit", (e) => {
         e.preventDefault();
         // Menghilakan Tombol Kirim
         btnLoading.classList.toggle('d-none');
         btnKirim.classList.toggle('d-none');
+        btnSubrek.classList.toggle('d-none');
         fetch(scriptURL, { method: "POST", body: new FormData(form) })
           .then((response) => {
             console.log("Success!", response)
             myAlert.classList.toggle('d-none')
             btnLoading.classList.toggle('d-none')
             btnKirim.classList.toggle('d-none')
+            btnSubrek.classList.toggle('d-none')
             form.reset();
           })
           .catch((error) => console.error("Error!", error.message));
